f0ae29d04d61998ff7758cbf814b62c4c60c3ea6
Simon Ser <contact@emersion.fr>
2024-08-07 15:46:07 +0200

    hack: allow times to be unset

--- src/app/models/trainrunsection.model.ts
+++ src/app/models/trainrunsection.model.ts
@@ -175,6 +175,9 @@ export class TrainrunSection {
   }
 
   private static formatDisplayText(time: TimeLockDto, offset: number): string {
+    if (time.time === null) {
+      return "?";
+    }
     if (!time?.timeFormatter?.stylePattern) {
       return undefined;
     }
