57977337b21541f2f1e832c526e2ce2c725b8f6d
Louis Greiner <57864277+louisgreiner@users.noreply.github.com>
2024-12-04 11:32:56 +0100

    hack: disable trainrunSection warnings (#6)

--- src/app/models/trainrunsection.model.ts
+++ src/app/models/trainrunsection.model.ts
@@ -6,6 +6,7 @@ import {ColorRefType, TimeLockDto, WarningDto} from "../data-structures/technica
 import {TrainrunSectionValidator} from "../services/util/trainrunsection.validator";
 import {formatDate} from "@angular/common";
 import {TrainrunsectionHelper} from "../services/util/trainrunsection.helper";
+import {environment} from "src/environments/environment";
 
 export class TrainrunSection {
   private static currentId = 0;
@@ -543,6 +544,7 @@ export class TrainrunSection {
   }
 
   hasTravelTimeWarning(): boolean {
+    if (environment.disableBackend) return false;
     return this.travelTime.warning !== null;
   }
 
@@ -551,18 +553,22 @@ export class TrainrunSection {
   }
 
   hasSourceDepartureWarning(): boolean {
+    if (environment.disableBackend) return false;
     return this.sourceDeparture.warning !== null;
   }
 
   hasSourceArrivalWarning(): boolean {
+    if (environment.disableBackend) return false;
     return this.sourceArrival.warning !== null;
   }
 
   hasTargetDepartureWarning(): boolean {
+    if (environment.disableBackend) return false;
     return this.targetDeparture.warning !== null;
   }
 
   hasTargetArrivalWarning(): boolean {
+    if (environment.disableBackend) return false;
     return this.targetArrival.warning !== null;
   }
 
