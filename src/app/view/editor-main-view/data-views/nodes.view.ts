7f70a514fb05440164eca863cc044cb410bbeca1
Louis Greiner <57864277+louisgreiner@users.noreply.github.com>
2025-01-27 17:12:53 +0100

    hack: hide  node connection time components (#9)
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>

--- src/app/view/editor-main-view/data-views/nodes.view.ts
+++ src/app/view/editor-main-view/data-views/nodes.view.ts
@@ -170,7 +170,7 @@ export class NodesView {
     this.makeAnalyticsTextLeftArea(groupEnter);
     this.makeAnalyticsTextRightArea(groupEnter);
     this.makeLabelText(groupEnter);
-    this.makeLabelConnectionText(groupEnter);
+    // this.makeLabelConnectionText(groupEnter);
   }
 
   makeNodeLODLevel3(groupEnter: any) {
@@ -183,7 +183,7 @@ export class NodesView {
     this.makeAnalyticsTextLeftArea(groupEnter);
     this.makeAnalyticsTextRightArea(groupEnter);
     this.makeLabelText(groupEnter);
-    this.makeLabelConnectionText(groupEnter);
+    // this.makeLabelConnectionText(groupEnter);
   }
 
   makeNodeLODLevel2(groupEnter: any) {
@@ -194,7 +194,7 @@ export class NodesView {
     this.makeNodeDockable(groupEnter);
     this.makeAnalyticsArea(groupEnter);
     this.makeLabelText(groupEnter);
-    this.makeLabelConnectionText(groupEnter);
+    // this.makeLabelConnectionText(groupEnter);
   }
 
   makeNodeLODLevel1(groupEnter: any) {
