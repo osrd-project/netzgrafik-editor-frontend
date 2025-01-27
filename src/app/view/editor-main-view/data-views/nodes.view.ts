3002d0e81062db6fdf7fe8c7f685da74e88186de
Louis Greiner <57864277+louisgreiner@users.noreply.github.com>
2025-01-27 16:19:00 +0100

    hack: disable node stopping times (#8)
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>

--- src/app/view/editor-main-view/data-views/nodes.view.ts
+++ src/app/view/editor-main-view/data-views/nodes.view.ts
@@ -1099,6 +1099,7 @@ export class NodesView {
   }
 
   private getNodeLabelTextWidth(node: Node): number {
+    return node.getNodeWidth() - NODE_TEXT_LEFT_SPACING;
     const connectionTime = node.getConnectionTime();
     let width = 0;
     if (connectionTime !== null) {
