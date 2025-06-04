ac1ba9eafa0271a4856509f5ca83f77a26eb94ab
Louis Greiner <greiner.louis@gmail.com>
2025-06-04 16:45:25 +0200

    hack: remove category shortName from trainrun name displays
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>

--- src/app/view/editor-main-view/data-views/trainrunsections.view.ts
+++ src/app/view/editor-main-view/data-views/trainrunsections.view.ts
@@ -470,10 +470,7 @@ export class TrainrunSectionsView {
   }
 
   static extractTrainrunName(trainrunSection: TrainrunSection): string {
-    return (
-      trainrunSection.getTrainrun().getCategoryShortName() +
-      trainrunSection.getTrainrun().getTitle()
-    );
+    return trainrunSection.getTrainrun().getTitle();
   }
 
   static extractTravelTime(trainrunSection: TrainrunSection, editorView: EditorView): string {
