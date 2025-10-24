e8baa5780c040eb26d54670209778fe2ef3c6f7c
Louis Greiner <greiner.louis@gmail.com>
2025-10-24 17:58:44 +0200

    asymmetry: helper functions for left/right <-> source/departure symmetry
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>

--- src/app/services/data/trainrunsection.service.ts
+++ src/app/services/data/trainrunsection.service.ts
@@ -1243,6 +1243,12 @@ export class TrainrunSectionService implements OnDestroy {
     });
   }
 
+  isTrainrunSymmetric(trainrunId: number): boolean {
+    return this.getAllTrainrunSectionsForTrainrun(trainrunId).every((section) =>
+      section.isSymmetric(),
+    );
+  }
+
   private copyTrainrunSection(
     existingTrainrunSection: TrainrunSection,
     newTrainrunId: number,
