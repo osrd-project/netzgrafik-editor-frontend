253ca3b529236e9f143cb99632eafb9a89893b13
Louis Greiner <louis.greiner@proton.me>
2026-07-06 14:07:54 +0200

    fixup! hack: allow times to be unset

--- src/app/data-structures/technical.data.structures.ts
+++ src/app/data-structures/technical.data.structures.ts
@@ -109,10 +109,10 @@ export interface TimeFormatter {
  */
 export interface TimeLockDto {
   time: number | null; // minutes [0..60]
-  consecutiveTime: number; // automatically updated after any data changes in the application
+  consecutiveTime: number | null; // automatically updated after any data changes in the application
   lock: boolean; // used to stop the time propagation (forward/backward)
-  warning?: WarningDto; // warning - if business logic detects an issue -> set human-readable warning
-  timeFormatter?: TimeFormatter; // undefined or object - optional
+  warning?: WarningDto | null; // warning - if business logic detects an issue -> set human-readable warning
+  timeFormatter?: TimeFormatter | null; // undefined or object - optional
 }
 
 /**
