98756e4b09faa459a201c5b63b9b6fedbcdbd647
Louis Greiner <louis.greiner@proton.me>
2026-07-07 16:52:11 +0200

    chore: expose data structures types AND enums
    In order to downstream be able to use the number enums that are defined in
    the data-structure folders, we need to exports that.
    
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 import type {} from "@angular/localize/init";
 
-export type * from "./app/data-structures/technical.data.structures";
-export type * from "./app/data-structures/business.data.structures";
+export * from "./app/data-structures/technical.data.structures";
+export * from "./app/data-structures/business.data.structures";
 export type * from "./app/models/operation.model";
